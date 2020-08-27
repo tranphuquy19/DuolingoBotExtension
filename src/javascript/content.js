a060e45b9995861dd98681bdb76358c0605c1ecc
Quy <tranphuquy19@gmail.com>
2020-08-28 00:43:22 +0700

    chotto

--- src/javascript/content.js
+++ src/javascript/content.js
@@ -1,5 +1,59 @@
 chrome.runtime.sendMessage({ todo: "activeApplication" });
 
+const dataTestComponentClassName = "e4VJZ";
+
+const CHARACTER_SELECT = "characterSelect";
+const CHARACTER_MATCH = "characterMatch";
+const TRANSLATE = "translate";
+const LISTEN_TAP = "listenTap";
+const NAME = "name";
+const COMPLETE_REVERSE_TRANLATION = "completeReverseTranslation";
+const LISTEN = "listen";
+
+function getChallenge() {
+    const dataTestDOM = document.getElementsByClassName(dataTestComponentClassName)[0];
+    const dataTestAtrr = Object.keys(dataTestDOM).filter(att => /^__reactInternalInstance/g.test(att))[0];
+    const childDataTestProps = dataTestDOM[dataTestAtrr].memoizedProps.children.props;
+    const { challenge } = childDataTestProps;
+    return challenge;
+}
+
+function solveChallenge() {
+    const challenge = getChallenge();
+    switch (challenge.type) {
+        case CHARACTER_SELECT:
+            const { choices, correctIndex } = challenge;
+            return { choices, correctIndex }; // trắc nghiệm 1 đáp án
+
+        case CHARACTER_MATCH:
+            const { pairs } = challenge;
+            return pairs; // tập hợp các cặp thẻ
+
+        case TRANSLATE: // nhập đán án
+        case NAME:
+            const { correctSolutions } = challenge;
+            return correctSolutions[0]; // nhập đáp án
+
+        case COMPLETE_REVERSE_TRANLATION:
+            const { displayTokens } = challenge;
+            return displayTokens.filter(token => token.isBlank)[0]; // điền vào từ còn thiếu
+
+        case LISTEN_TAP:
+        case LISTEN:
+            const { prompt } = challenge;
+            return prompt; // nghe và điền vào ô input
+
+        default:
+            break;
+    }
+    // document.querySelector("#root > div > div > div > div > div._2Fc1K > div > div > div._10vOG > button").click()
+    // document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode':13, 'which':13}))
+}
+
+window.getChallenge = getChallenge;
+window.solveChallenge = solveChallenge;
+
 chrome.runtime.onMessage.addListener(message => {
     console.log(message);
+    solveChallenge();
 });
--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,12 +1,22 @@
-const dataTestComponentClassName = "e4VJZ";
+const dataTestComponentClassName = 'e4VJZ';
 
-const CHARACTER_SELECT = "characterSelect";
-const CHARACTER_MATCH = "characterMatch";
-const TRANSLATE = "translate";
-const LISTEN_TAP = "listenTap";
-const NAME = "name";
-const COMPLETE_REVERSE_TRANLATION = "completeReverseTranslation";
-const LISTEN = "listen";
+const TIME_OUT = 750;
+
+const CHARACTER_SELECT = 'characterSelect';
+const CHARACTER_MATCH = 'characterMatch';
+const TRANSLATE = 'translate';
+const LISTEN_TAP = 'listenTap';
+const NAME = 'name';
+const COMPLETE_REVERSE_TRANLATION = 'completeReverseTranslation';
+const LISTEN = 'listen';
+const SELECT = 'select';
+const JUDGE = 'judge';
+
+const CHALLENGE_CHOICE_CARD = '[data-test="challenge-choice-card"]';
+const CHALLENGE_TRANSLATE_INPUT = '[data-test="challenge-translate-input"]';
+const CHALLENGE_JUDGE_TEXT = '[data-test="challenge-judge-text"]';
+const CHALLENGE_TEXT_INPUT = '[data-test="challenge-text-input"]';
+const PLAYER_NEXT = '[data-test="player-next"]';
 
 function getChallenge() {
     const dataTestDOM = document.getElementsByClassName(dataTestComponentClassName)[0];
@@ -16,36 +26,96 @@ function getChallenge() {
     return challenge;
 }
 
-function solveChallenge() {
+function pressEnter() {
+    document.dispatchEvent(new KeyboardEvent('keydown', { 'keyCode': 13, 'which': 13 }));
+}
+
+function dynamicInput(element, msg) {
+    let input = element;
+    let lastValue = input.value;
+    input.value = msg;
+    let event = new Event('input', { bubbles: true });
+    // hack React15
+    event.simulated = true;
+    // hack React16 内部定义了descriptor拦截value，此处重置状态
+    let tracker = input._valueTracker;
+    if (tracker) {
+        tracker.setValue(lastValue);
+    }
+    input.dispatchEvent(event);
+}
+
+function classify() {
+    const clickEvent = new MouseEvent('click', {
+        view: window,
+        bubbles: true,
+        cancelable: true
+    });
     const challenge = getChallenge();
     switch (challenge.type) {
-        case CHARACTER_SELECT:
+        case SELECT:
+        case CHARACTER_SELECT: { // trắc nghiệm 1 đáp án
             const { choices, correctIndex } = challenge;
-            return { choices, correctIndex }; // trắc nghiệm 1 đáp án
+            document.querySelectorAll(CHALLENGE_CHOICE_CARD)[correctIndex].dispatchEvent(clickEvent);
+            return { choices, correctIndex };
+        }
 
-        case CHARACTER_MATCH:
+        case CHARACTER_MATCH: {// tập hợp các cặp thẻ
             const { pairs } = challenge;
-            return pairs; // tập hợp các cặp thẻ
+            return pairs;
+        }
 
-        case TRANSLATE: // nhập đán án
-        case NAME:
+        case TRANSLATE:
+        case NAME: { // nhập đán án
             const { correctSolutions } = challenge;
-            return correctSolutions[0]; // nhập đáp án
+            let textInputElement = document.querySelectorAll(CHALLENGE_TRANSLATE_INPUT)[0];
+            let correctSolution = correctSolutions[0];
+            dynamicInput(textInputElement, correctSolution);
+            return correctSolutions[0];
+        }
 
-        case COMPLETE_REVERSE_TRANLATION:
+        case COMPLETE_REVERSE_TRANLATION: { // điền vào từ còn thiếu
             const { displayTokens } = challenge;
-            return displayTokens.filter(token => token.isBlank)[0]; // điền vào từ còn thiếu
+            const { text } = displayTokens.filter(token => token.isBlank)[0];
+            let textInputElement = document.querySelectorAll(CHALLENGE_TEXT_INPUT)[0];
+            dynamicInput(textInputElement, text);
+            return text;
+        }
 
         case LISTEN_TAP:
-        case LISTEN:
+        case LISTEN: { // nghe và điền vào ô input
             const { prompt } = challenge;
-            return prompt; // nghe và điền vào ô input
+            let textInputElement = document.querySelectorAll(CHALLENGE_TRANSLATE_INPUT)[0];
+            dynamicInput(textInputElement, prompt);
+            return prompt;
+        }
+
+        case JUDGE: { // trắc nghiệm 1 đáp án
+            const { correctIndices } = challenge;
+            document.querySelectorAll(CHALLENGE_JUDGE_TEXT)[correctIndices[0]].dispatchEvent(clickEvent);
+            return correctIndices;
+        }
 
         default:
             break;
     }
-    // document.querySelector("#root > div > div > div > div > div._2Fc1K > div > div > div._10vOG > button").click()
-    // document.dispatchEvent(new KeyboardEvent('keydown', {'keyCode':13, 'which':13}))
 }
 
-solveChallenge();
+function main() {
+    try {
+        let isPlayerNext = document.querySelectorAll(PLAYER_NEXT)[0].textContent.toUpperCase();
+        if (isPlayerNext.valueOf() !== 'CONTINUE') {
+            classify();
+            pressEnter();
+        }
+        setTimeout(pressEnter, 150);
+    } catch (e) {
+        return e;
+    }
+}
+
+function solveChallenge(autoSolve = true) {
+    setInterval(main, TIME_OUT);
+}
+
+// solveChallenge();
