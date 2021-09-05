df9a6334399217dbaf7de28dc23c8b5a9ab7ccbd
tranphuquy19 <christian@enclave.vn>
2021-09-05 19:42:05 +0700

    create tampermonkey scrypt

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 // @name         Duolingo-Cheat-Tool
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
-// @author       You
+// @description  Auto answer Duolingo script!
+// @author       Tran Quy <tranphuquy19@gmail.com>
 // @match        https://www.duolingo.com/skill*
 // @icon         https://www.google.com/s2/favicons?domain=duolingo.com
 // @grant        none
--- readme.md
+++
@@ -1,3 +0,0 @@
-Demo Video
-
-[![DEMO](http://i3.ytimg.com/vi/I9nhnlCUDx8/maxresdefault.jpg)](https://www.youtube.com/watch?v=I9nhnlCUDx8 "Duolingo Cheat All Languages - Demo")
